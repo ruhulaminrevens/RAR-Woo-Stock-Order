677a2657c89169ad9f739496be01cbfeee430fd7
Ruhul Amin Revens <52210203+ruhulaminrevens@users.noreply.github.com>
2026-09-24 21:00:10 +0600

    fix: correct HPOS namespace bootstrap

--- rar-woo-stock-order.php
+++ rar-woo-stock-order.php
@@ -29,7 +29,7 @@ register_deactivation_hook( __FILE__, array( 'RAR_WSO_Plugin', 'deactivate' ) );
 
 add_action( 'before_woocommerce_init', static function () {
     if ( class_exists( '\\Automattic\\WooCommerce\\Utilities\\FeaturesUtil' ) ) {
-        \\Automattic\\WooCommerce\\Utilities\\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+        \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
     }
 } );
 
